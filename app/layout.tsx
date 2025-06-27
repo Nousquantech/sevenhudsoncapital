2843605fc90cc71edce19f454a8cf705a43ae0f1
Nousquantech <143766008+Nousquantech@users.noreply.github.com>
2025-06-27 08:05:18 -0400

    first try

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${EB_GaramondFont.className}  antialiased`}>
-        <Menu />
         {children}
         <Footer />
       </body>
